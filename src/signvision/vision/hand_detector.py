af612ad0b462eceeab51168f470310b473100b91
sebastian <nugrasebas2@gmail.com>
2026-08-18 22:30:38 -0500

    feat: initialize MediaPipe Hands in HandDetector

--- src/signvision/vision/hand_detector.py
+++ src/signvision/vision/hand_detector.py
@@ -1,7 +1,8 @@
+import mediapipe as mp
+
+
 class HandDetector:
-    """
-    Responsible for managing hand detection.
-    """
+    """Responsible for managing hand detection."""
 
     def __init__(self) -> None:
-        self._hands = None
+        self._hands = mp.solutions.hands.Hands()
