ad34fc698da0bdcb58ef3beae2d1504b16e10ac9
sebastian <nugrasebas2@gmail.com>
2026-09-25 11:19:14 -0500

    feat: export training classes from training package

--- src/signvision/training/__init__.py
+++ src/signvision/training/__init__.py
@@ -4,3 +4,15 @@ Training Module
 Responsibility: Manage landmark data collection, dataset persistence,
 and AI model training.
 """
+
+from .data_collector import DataCollector
+from .dataset import LandmarkDataset, load_dataset, save_dataset
+from .trainer import LandmarkTrainer
+
+__all__ = [
+    "DataCollector",
+    "LandmarkDataset",
+    "LandmarkTrainer",
+    "load_dataset",
+    "save_dataset",
+]
